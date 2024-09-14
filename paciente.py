33e3e5ed3b5e862b70e7c4987707c462f27b31b8
Carlos Gama <l22121094@morelia.tecnm.mx>
2024-09-13 22:39:33 -0600

    Codigo de clase modificado, hasta el 13 de septiembre

--- paciente.py
+++ paciente.py
@@ -9,10 +9,18 @@ class  Paciente:
     direccion = ""
 
     def __init__(self, nombre, ano_nacimiento, peso, estatura, direccion):
-    
-     self.id = random.randint(1, 1000)
-     self.nombre = nombre
-     self.ano_nacimiento = ano_nacimiento 
-     self.peso = peso
-     self.estatura = estatura
-     self.direccion = direccion
+      self.id = random.randint(1, 1000)
+      self.nombre = nombre
+      self.ano_nacimiento = ano_nacimiento 
+      self.peso = peso
+      self.estatura = estatura
+      self.direccion = direccion
+
+
+    def mostrar_informacion(self):
+        for paciente in self.pacientes:
+          print(f"\nId: {paciente.id}")
+          print(f"\nNombre: {paciente.nombre}")
+          print(f"\nPeso: {paciente.peso}")
+          print(f"\nEstatura: {paciente.estatura}")
+          print(f"\nDireccion: {paciente.direccion}")
